96d4d4b00a4650525ed2f7aebcc251b62d5861d5
cguduru <33423957+cguduru@users.noreply.github.com>
2017-12-29 22:38:23 +0400

    Add files via upload

---
+++ dominoCapstone/installpack.R
@@ -0,0 +1,24 @@
+################# ~~~~~~~~~~~~~~~~~ ######## ~~~~~~~~~~~~~~~~~ #################
+##                                                                            ##
+##                        Data Science Capstone Project                       ##
+##                                                                            ##            
+##                               Chandra Guduru                               ##
+##                                                                            ##
+##                                                                            ##
+##                                                                            ##
+################# ~~~~~~~~~~~~~~~~~ ######## ~~~~~~~~~~~~~~~~~ #################
+
+install.packages(c("h2o",
+"RWekajars",
+"ggplot2",
+"qdapDictionaries",
+"qdapRegex",
+"qdapTools",
+"RColorBrewer",
+"qdap",
+"NLP",
+"tm",
+"SnowballC",
+"slam",
+"RWeka",
+"rJava"))
